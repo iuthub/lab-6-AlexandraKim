c0d2bcc1647cd7c3659b64f8c34d516378444064
Alexandra Kim <alexandra.success,kim@gmail.com>
2020-03-24 02:53:16 +0500

    part 1

--- webpage/regex_valid_form.php
+++ webpage/regex_valid_form.php
@@ -1,5 +1,14 @@
 <?php
-
+// /(fox)/
+// Email /^[a-zA-Z0-9\._]{4,}@[a-zA-Z0-9]{2,}\.\w{2,}$/
+// Phone number /^\+998-\d{2}-\d{7}$/
+// preg_replace("/\s+/", "", "The quick " " brown fox")
+// preg_replace("/[^0-9^,^\.]/", "", "$123,34.00A")
+// preg_replace("/\n/", "", "Twinkle, twinkle, little star,
+// How I wonder what you are.
+// Up above the world so high,
+// Like a diamond in the sky.)
+// preg_match("/\[\w+\]/", "The quick brown [fox]", $matches)
 	$pattern="";
 	$text="";
 	$replaceText="";
@@ -13,6 +22,12 @@ if ($_SERVER["REQUEST_METHOD"]=="POST") {
 	$replaceText=$_POST["replaceText"];
 
 	$replacedText=preg_replace($pattern, $replaceText, $text);
+
+	preg_match($pattern, $text, $matches);
+	for ($i=0; $i < count($matches); $i++) { 
+		$matches[$i] = preg_replace("/[\[\]]/", "", $matches[$i]);
+	}
+	print_r($matches);
 
 	if(preg_match($pattern, $text)) {
 						$match="Match!";
